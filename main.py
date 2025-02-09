9ebcd1e6214bf45ddd4a0be7c615ffc0185336c9
Raivat Alwar <100984683+rai12324@users.noreply.github.com>
2025-02-09 18:04:30 -0500

    Update main.py
    Added lines in the print statements to view the count of generated nodes and expanded nodes.

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ if __name__ == "__main__":
     initial_state, goal_state = get_user_input()
     
     print("\n Solving with Manhattan Distance heuristic...")
-    solution = a_star(initial_state, goal_state, manhattan_distance)
+    solution, nodes_generated, nodes_expanded = a_star(initial_state, goal_state, manhattan_distance)
 
     if solution:
         print("Solution found!")
         print_solution(solution)
     else:
         print("No solution found.")
+    print(f"Manhattan Distance - Nodes Generated: {nodes_generated}, Nodes Expanded: {nodes_expanded}")
 
     print("\n Solving with Misplaced Tiles heuristic...")
-    solution = a_star(initial_state, goal_state, misplaced_tiles)
+    solution, nodes_generated, nodes_expanded = a_star(initial_state, goal_state, misplaced_tiles)
 
     if solution:
         print("Solution found!")
         print_solution(solution)
     else:
         print("No solution found.")
+    print(f"Misplaced Tiles - Nodes Generated: {nodes_generated}, Nodes Expanded: {nodes_expanded}")
